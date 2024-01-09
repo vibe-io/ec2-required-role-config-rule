cf20f3a48385108094ef5a0e2393bf3ff9f81413
Kevin Lucas <Pharrox@users.noreply.github.com>
2024-01-09 17:33:36 +0000

    chore: Configure reposiroty settings

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,11 +3,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Kevin Lucas',
   authorAddress: 'Pharrox@users.noreply.github.com',
   cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: 'master',
+  gitignore: [
+    '/.vscode/',
+  ],
   jsiiVersion: '~5.0.0',
   name: 'ec2-required-role-config-rule',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/Pharrox/ec2-required-role-config-rule.git',
+  repositoryUrl: 'https://github.com/vibe-io/ec2-required-role-config-rule.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
